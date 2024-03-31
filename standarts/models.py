27f6605179ecac2e4a8ac1e04bfc4d1ec608cab5
AaronVanguard <demidov.03@mail.ru>
2024-03-31 15:12:07 +0700

    added models to models.py

--- standarts/models.py
+++ standarts/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class Level(models.Model):
+    level = models.CharField(max_length=255)
+
+
+class Gender(models.Model):
+    gender = models.CharField(max_length=255)
+
+
+class Classes(models.Model):
+    number = models.IntegerField()
+
+
+class Students(models.Model):
+    classes = models.ForeignKey(Classes, on_delete=models.CASCADE)
+    gender = models.ForeignKey(Gender, on_delete=models.CASCADE)
+    recruitment_year = models.DateField()
+    class_name = models.CharField(max_length=255)
+    birthday = models.DateField()
+
+
+class Grades(models.Model):
+    grade = models.CharField(max_length=255)
+
+
+class Standards(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    has_numeric_value = models.BooleanField()
+
+
+class StandardsValues(models.Model):
+    standard = models.ForeignKey(Standards, on_delete=models.CASCADE)
+    classes = models.ForeignKey(Classes, on_delete=models.CASCADE)
+    gender = models.ForeignKey(Gender, on_delete=models.CASCADE)
+    level = models.ForeignKey(Level, on_delete=models.CASCADE)
+    value = models.FloatField()
+
+
+class StudentsStandards(models.Model):
+    student = models.ForeignKey(Students, on_delete=models.CASCADE)
+    standard = models.ForeignKey(Standards, on_delete=models.CASCADE)
+    classes = models.ForeignKey(Classes, on_delete=models.CASCADE)
+    grade = models.ForeignKey(Grades, on_delete=models.CASCADE)
+    value = models.FloatField()
